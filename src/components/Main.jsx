3e28a58e8fab322558e69926666b7c074962a75f
Santiago Crippa <santicrippa@gmail.com>
2023-05-03 19:08:51 -0300

    adding counter

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import { useState } from 'react';
 
 import '../App.css';
 const Main = () => {
+
+    let resultado = 0;
+
     return (
-        <div className='main'>
-            Main
-        </div>
+        <>
+            <div className='grid justify-center align-middle bg-red-500 m-2 p-4 gap-5 max-w-sm rounded-xl text-center'>
+                <p className='font-mono font-bold text-xl'>Counter</p>
+                <div className='font-mono font-bold text-xl'>
+                    {resultado}
+                </div>
+                <button 
+                className='bg-red-300 rounded-xl w-20 hover:bg-slate-700 hover:text-white'
+                >
+                    +1
+                </button>
+                <button 
+                className='bg-red-300 rounded-xl w-20 hover:bg-slate-700 hover:text-white'
+                >
+                    -1
+                </button>
+            </div>
+        </>
     )
 }
 
