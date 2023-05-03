ee45e46ab889df03eb2d3b41be23882b25091ea7
Santiago Crippa <santicrippa@gmail.com>
2023-05-03 00:32:55 -0300

    styling navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,11 +2,23 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <nav className='container max-w-full bg-orange-500 font-serif font-bold'>
-            <ul className='flex gap-5 justify-center'>
-                <li>Item</li>
-                <li>Item</li>
-                <li>Item</li>
+        <nav className='container max-w-full font-sans text-xl font-bold pt-2'>
+            <ul className='flex gap-7 justify-center text-slate-900 '>
+                <a 
+                href="/" 
+                className="hover:text-slate-50 hover:scale-125 duration-200">
+                Home
+                </a>
+                <a 
+                href="/" 
+                className="hover:text-slate-50 hover:scale-125 duration-200">
+                Products
+                </a>
+                <a 
+                href="/" 
+                className="hover:text-slate-50 hover:scale-125 duration-200">
+                Contact
+                </a>
             </ul>
         </nav>
     )
