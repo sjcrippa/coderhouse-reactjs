a75254a45586579a16a5f3a234a0c915495d2504
Santiago Crippa <santicrippa@gmail.com>
2023-05-05 01:54:29 -0300

    gettings states!!!!!!!!!

--- src/components/containers/ItemListContainer.jsx
+++ src/components/containers/ItemListContainer.jsx
@@ -1,12 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const ItemListContainer = () => {
 
+    const [saludar, setSaludar] = useState('Click for Greetings!');
+    const Saludando = () => {
+        setSaludar('Welcome!');
+    };
+
     return (
         <>
-            <div className='text-center text-lg font-[Verdana] '>ItemListContainer</div>
+            <div className='flex mx-auto justify-center mt-10'>
+                <p className='text-lg font-bold font-[Verdana] text-slate-900'>
+                    <button
+                        className='bg-red-500 shadow-2xl border-gray-900'
+                        onClick={() => {
+                            Saludando()
+                        }}>
+                        {saludar}
+                    </button>
+                </p>
+            </div>
         </>
-    )
-}
+    );
+};
 
-export default ItemListContainer
+export default ItemListContainer;
