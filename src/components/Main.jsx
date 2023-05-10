1031c873aa2e32f789db75b711b1e53c9a1b6da5
Santiago Crippa <santicrippa@gmail.com>
2023-05-10 00:50:55 -0300

    details and class 5

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ItemListContainer from './containers/ItemListContainer';
 import BtnForCounter from './containers/BtnForCounter';
 import Clase5 from './clases/clase5/Clase5';
+import Efectos from './clases/clase5/Efectos';
 
 const Main = () => {
 
@@ -11,6 +12,7 @@ const Main = () => {
             <ItemListContainer />
             <BtnForCounter />
             <Clase5 />
+            <Efectos />
         </>
     )
 }
