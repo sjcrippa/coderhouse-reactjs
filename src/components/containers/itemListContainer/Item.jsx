b29d2b9488cc8900be24742883725953e7d4387c
Santiago Crippa <santicrippa@gmail.com>
2023-05-18 12:22:06 -0300

    styling

--- src/components/containers/itemListContainer/Item.jsx
+++ src/components/containers/itemListContainer/Item.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from 'react';
 
 const Item = ({ producto }) => {
     return (
         <>
-            <div className='mt-10 card container mx-auto text-center'>
-                <div className='grid grid-cols-3 border-2 m-10 p-10 '>
+            <div className='mt-10 card container mx-auto text-center px-36 xl:px-[30rem]'>
+                <div className='border-2 p-5'>
                     <img
                         className='mx-auto'
                         src={producto.image}
@@ -14,10 +14,13 @@ const Item = ({ producto }) => {
                     <p>For {producto.gender}</p>
                     <p>$ {producto.price}</p>
                     <p>{producto.description}</p>
+                    <button className='mt-5 p-2 px-10 text-center bg-red-500 rounded-xl'>
+                        Add to cart
+                    </button>
                 </div>
             </div>
         </>
     )
 }
 
-export default Item
+export default Item;
--- src/components/containers/itemListContainer/ItemListContainer.jsx
+++ src/components/containers/itemListContainer/ItemListContainer.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 import ItemList from './ItemList';
 import pedirProductos from '../../../functions/pedirProductos';
+
 const ItemListContainer = () => {
     const [productos, setProductos] = useState([]);
 
