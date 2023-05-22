8564a44737f53d41fd6b1759fb1223e583f5db43
Santiago Crippa <santicrippa@gmail.com>
2023-05-22 18:31:18 -0300

    'styling'

--- src/components/containers/itemListContainer/Item.jsx
+++ src/components/containers/itemListContainer/Item.jsx
@@ -5,16 +5,18 @@ const Item = ({ producto }) => {
     return (
         <>
             <div className='mt-10 text-center'>
-                <div className='border-2 border-black p-5 m-10 rounded-xl shadow-lg shadow-black'>
+                <div className='border-2 border-black p-5 m-10 rounded-xl shadow-lg shadow-black h-80'>
                     <img
                         className='mx-auto'
                         src={producto.image}
                         alt={producto.name}
                     />
-                    <h2>{producto.name}</h2>
-                    <p>For {producto.gender}</p>
-                    <p>$ {producto.price}</p>
-                    <p>{producto.description}</p>
+                    <div className='mt-3'>
+                        <h2>{producto.name}</h2>
+                        <p>For {producto.gender}</p>
+                        <p>$ {producto.price}</p>
+                        <p>{producto.description}</p>
+                    </div>
                     <Counter />
                 </div>
             </div>
