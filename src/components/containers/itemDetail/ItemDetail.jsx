e02d0de5328623424fb09325d3f1c3e16b87fa82
Santiago Crippa <santicrippa@gmail.com>
2023-05-28 21:53:40 -0300

    litle more

--- src/components/containers/itemDetail/ItemDetail.jsx
+++ src/components/containers/itemDetail/ItemDetail.jsx
@@ -8,7 +8,7 @@ const ItemDetail = ({ item }) => {
             <div className='mx-auto'>
                 <div className='ml-5 flex'>
                     <div className='card mt-10 w-1/3 md:w-1/4 xl:w-1/5 rounded-xl shadow-lg bg-white shadow-black hover:shadow-2xl hover:shadow-black duration-150'>
-                        <img className='rounded-t-xl' src={item.image} alt="prod-img" />
+                        <img className='p-1 rounded-t-xl' src={item.image} alt="prod-img" />
                         <div className='p-1 mt-1 text-sm lg:text-base text-center font-serif font-semibold'>
                             <h3 className='uppercase text-lg'>{item.name}</h3>
                             <p className='mt-3 text-start ml-2'>$ {item.price}</p>
@@ -21,6 +21,7 @@ const ItemDetail = ({ item }) => {
                         <h2>ORDER IT  NOW!</h2>
                         <p>VISA</p>
                         <p>MASTERCARD</p>
+                        <p>HOME DELIVER</p>
                         <div className='mt-4'>
                             <Counter />
                         </div>
