1cfc631c55ab946307291343bdb3f764db44322c
Santiago Crippa <santicrippa@gmail.com>
2023-06-01 00:47:15 -0300

    styling and more stylingggggggg

--- src/components/containers/brand/BrandTitle.jsx
+++ src/components/containers/brand/BrandTitle.jsx
@@ -5,8 +5,8 @@ import BrandBtns from './BrandBtns';
 const BrandTitle = () => {
     return (
         <>
-            <div className='flex flex-col mx-auto text-center text-white cursor-default'>
-                <h1 className='mt-16 md:mt-5 text-6xl md:text-7xl lg:text-8xl xl:text-[10rem]'>
+            <div className=' flex flex-col mx-auto text-center text-white cursor-default'>
+                <h1 className='md:p-16 xl:p-12 mt-32 md:mt-5 text-6xl md:text-7xl lg:text-8xl xl:text-[10rem]'>
                     paco rabanne
                 </h1>
                 <h2 className='mt-56 md:mt-72 xl:mt-60 text-4xl md:text-5xl lg:text-6xl uppercase'>
--- src/components/containers/navbar/Navbar.jsx
+++ src/components/containers/navbar/Navbar.jsx
@@ -6,9 +6,11 @@ import { AiOutlineClose } from 'react-icons/ai'
 import { pacoLogo } from '../../../assets';
 import CartWidget from './CartWidget';
 import NavItems from './NavItems';
+import '../../../App.css';
 
 const Navbar = () => {
     const [menu, setMenu] = useState(true);
+    const [blur, setBlur] = useState(true);
 
     const handleMenu = () => {
         setMenu(!menu)
@@ -18,12 +20,18 @@ const Navbar = () => {
         !menu && setMenu(false)
     };
 
+    const handleBlur = () => {
+        !blur && setBlur(false)
+    }
+
     return (
         <>
             <div className='relative'>
                 <div className={menu ? 'left-[-100%]' : 'absolute top-0 left-0 w-full h-[1990vh]  bg-black/50 z-0 md:hidden'}>
                 </div>
-                <nav className='flex items-center h-16 p-6 bg-white w-full'>
+                <nav 
+                onChange={handleBlur}
+                className={!blur ?  'blur-nav fixed flex items-center h-16 p-6 bg-white bg-opacity-80 bg-clip-padding w-full' : 'blur-nav fixed flex items-center h-16 p-6 bg-white w-full' }>
                     <Link to={"/"}>
                         <img
                             className='w-36 sm:min-w-36'
@@ -51,21 +59,21 @@ const Navbar = () => {
                     <CartWidget />
                 </nav>
 
-                <nav className={!menu ? 'fixed left-0 top-0 w-[40%] bg-white h-full border-r-2 border-black/20 ease-in-out duration-500 backdrop-opacity-50 md:hidden' : 'fixed left-[-100%]'} >
+                <nav className={!menu ? 'fixed mt-16 left-0 w-[40%] bg-white opacity-90 h-full ease-in-out duration-700 backdrop-opacity-50 md:hidden' : 'fixed left-[-100%]'} >
                     <ul className='uppercase flex flex-col'>
                         <Link
                             to={"/productos/him"}
-                            className="p-4 pl-4 hover:text-white hover:bg-slate-900 duration-300 md:p-0 text-md md:text-base font-extralight border-b-2 border-black ">
+                            className="nav-btn p-4 pl-4 hover:text-white hover:bg-slate-900 duration-300 md:p-0 text-md md:text-base font-extralight border-b-2 border-black w-36">
                             FOR HIM
                         </Link>
                         <Link
                             to={"/productos/her"}
-                            className="p-4 pl-4 hover:text-white hover:bg-slate-900 duration-300 md:p-0 text-md md:text-base font-extralight border-b-2 border-black ">
+                            className="nav-btn p-4 pl-4 hover:text-white hover:bg-slate-900 duration-300 md:p-0 text-md md:text-base font-extralight border-b-2 border-black w-36">
                             FOR HER
                         </Link>
                         <Link
                             to={"/productos"}
-                            className="p-4 pl-4 hover:text-white hover:bg-slate-900 duration-300 md:p-0 text-md md:text-base font-extralight border-b-2 border-black ">
+                            className="nav-btn p-4 pl-4 hover:text-white hover:bg-slate-900 duration-300 md:p-0 text-md md:text-base font-extralight border-b-2 border-black w-36">
                             ALL
                         </Link>
                     </ul>
