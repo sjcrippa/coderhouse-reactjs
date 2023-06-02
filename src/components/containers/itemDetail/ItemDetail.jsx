9e9e06063341e8cb078a057018887a70c9fdf370
Santiago Crippa <santicrippa@gmail.com>
2023-06-02 18:38:03 -0300

    responsive

--- src/components/containers/itemDetail/ItemDetail.jsx
+++ src/components/containers/itemDetail/ItemDetail.jsx
@@ -17,9 +17,9 @@ const ItemDetail = ({ item }) => {
     }
     return (
         <>
-            <div className='py-16 mx-auto'>
-                <div className='ml-8 flex'>
-                    <div className='card mt-10 w-1/3 md:w-1/4 xl:w-1/5 rounded-xl shadow-lg bg-white shadow-black hover:shadow-2xl hover:shadow-black duration-150'>
+            <div className='py-16 mx-auto '>
+                <div className='ml-8 block sm:flex'>
+                    <div className='card mt-10 w-4/6 mx-auto sm:w-1/6 xl:w-1/5 rounded-xl shadow-lg bg-white shadow-black hover:shadow-2xl hover:shadow-black duration-150'>
                         <img className='p-1 rounded-t-xl' src={item.image} alt="prod-img" />
                         <div className='p-1 mb-1 text-sm lg:text-base text-center font-serif font-semibold'>
                             <h3 className='uppercase text-lg border-b-2 border-black'>{item.name}</h3>
@@ -29,7 +29,7 @@ const ItemDetail = ({ item }) => {
                         </div>
                     </div>
 
-                    <div className='mt-10 m-5 mx-4 md:mx-8 rounded-xl bg-white p-5 h-56 shadow-lg shadow-black'>
+                    <div className='mt-10 m-5 mx-16 md:mx-8 rounded-xl bg-white p-3 shadow-lg shadow-black'>
                         <h2>ORDER IT  NOW!</h2>
                         <p>VISA</p>
                         <p>MASTERCARD</p>
