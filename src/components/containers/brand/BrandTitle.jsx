73a311854d6827822515083da8d5793cbb7abefe
Santiago Crippa <santicrippa@gmail.com>
2023-06-01 21:13:00 -0300

    responsive

--- src/components/containers/brand/BrandTitle.jsx
+++ src/components/containers/brand/BrandTitle.jsx
@@ -9,7 +9,7 @@ const BrandTitle = () => {
                 <h1 className='md:p-16 xl:p-12 mt-24 md:mt-5 text-6xl md:text-7xl lg:text-8xl xl:text-[10rem]'>
                     paco rabanne
                 </h1>
-                <h2 className='mt-40 md:mt-24 text-4xl md:text-5xl lg:text-6xl uppercase'>
+                <h2 className='mt-64 md:mt-24 text-4xl md:text-5xl lg:text-6xl uppercase'>
                     nuestros nuevos e icónicos lanzamientos
                 </h2>
             </div>
