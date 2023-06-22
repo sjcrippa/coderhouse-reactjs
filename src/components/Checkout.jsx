973543fbb40217b6f9d6eac740cb0163d89092bb
Santiago Crippa <santicrippa@gmail.com>
2023-06-22 19:08:38 -0300

    adding form

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 
+import '../App.css'
 const Checkout = () => {
     return (
         <>
             <div className='container mx-auto p-5'>
-                <div className='w-full h-screen bg-black text-center'>
+                <div className='w-full h-screen text-center'>
                     <div className='relative top-24'>
-                        <h1 className='text-white text-2xl'>Checkout</h1>
+                        <h1 className='text-2xl'>Checkout</h1>
+                        <form className='mt-5'>
+                            <div className='flex flex-col gap-3'>
+                                <label className='text-start' htmlFor="">First and last name</label>
+                                <input className='z-0' type="text" placeholder='name' />
+                                
+                                <label className='text-start' htmlFor="">Email</label>
+                                <input type="email" placeholder='email' />
+                                
+                                <label className='text-start' htmlFor="">Phone number</label>
+                                <input type="tel" placeholder='phone' />
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
